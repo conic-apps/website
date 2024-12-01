bb33533c163d596b6a580e3cfa544d3ca1b927a4
Broken_Deer <64290747+Broken-Deer@users.noreply.github.com>
2024-12-01 11:28:47 +0800

    ci: add audit, lint and test (#2)
    * ci: add audit, lint and test
    
    * fix audit
    
    * fix audit

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,9 +1,3 @@
 // Amethyst Launcher Website
 // Copyright 2022-2025 Broken-Deer and contributors. All rights reserved.
 // SPDX-License-Identifier: GPL-3.0-only
-
-mod download;
-mod home;
-
-pub use download::download_template;
-pub use home::home_template;
